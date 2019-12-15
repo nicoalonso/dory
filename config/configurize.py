1d62e61315baef9e3a7147b572b901fa4ab96354
Nico <nicolas.alonso81@gmail.com>
2019-12-15 12:26:11 +0100

    Update gitignore

--- config/configurize.py
+++ config/configurize.py
@@ -23,15 +23,5 @@ class Configurize:
 
 
 if __name__ == '__main__':
-
-    MsgTerm.message('message')
-    MsgTerm.text('text')
-    MsgTerm.debug('debug')
-    MsgTerm.info('info')
-    MsgTerm.warning('warning')
-    MsgTerm.alert('alert')
-    MsgTerm.error('error')
-    MsgTerm.fatal('fatal')
-
-    # cfg = Configurize('Test')
-    # cfg.load()
+    cfg = Configurize('Test')
+    cfg.load()
