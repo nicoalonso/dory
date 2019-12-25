fdde86d3c96d10f1c0bbe019c39225925dbcc27a
Nico <nicolas.alonso81@gmail.com>
2019-12-25 11:35:20 +0100

    Docstring like as Google Style

--- utils/FormHtmlParser.py
+++ utils/FormHtmlParser.py
@@ -1,29 +1,56 @@
-# Class for parser html form
+#!/usr/bin/python3
 
 from html.parser import HTMLParser
 
-# Parser Form
-# @see https://docs.python.org/3/library/html.parser.html
-# 
+
 class FormHtmlParser(HTMLParser):
-    # Form inputs
+    '''Form parser
+    
+    Parse html to get the forms
+    @doc https://docs.python.org/3/library/html.parser.html
+    
+    Extends:
+        HTMLParser
+    
+    Constants:
+        INPUTS {list}: form tags to find
+
+    Attributes:
+        in_form {bool}: flag to know if we are in a form
+        current_form {string}: name of the current form
+        forms {dict}: store forms and their inputs
+    '''
     INPUTS = ['input', 'select', 'textarea']
 
-    # Constructor
     def __init__(self):
         HTMLParser.__init__(self)
         self.in_form = False
         self.current_form = ''
         self.forms = {}
 
-    # Get Form
+
     def getForm(self, name):
+        '''Get form by name
+        
+        Arguments:
+            name {string}: form name
+        
+        Returns:
+            dict|None
+        '''
         if name in self.forms:
             return self.forms[name]
         return None
 
-    # new form
-    def addForm(self, attrs):
+
+    def _add_form(self, attrs):
+        '''add form
+        
+        private method, add form to dictionary
+        
+        Arguments:
+            attrs {dict}: form attributes
+        '''
         frm = { name: value for (name, value) in attrs }
         frm['inputs'] = {}
 
@@ -40,20 +67,27 @@ class FormHtmlParser(HTMLParser):
         self.forms[name] = frm
         self.current_form = name
 
-    # Add inputs to form
-    def addInput(self, attrs):
+
+    def _add_input(self, attrs):
+        '''Add input to form
+
+        private method
+        
+        Arguments:
+            attrs {dict}: input attributes
+        '''
         field = { name: value for (name, value) in attrs }
         
         if 'name' in field:
             self.forms[self.current_form]['inputs'][field['name']] = field
 
-    # Handlers
+    # Handlers inherit from HTMLParser
     def handle_starttag(self, tag, attrs):
         if self.in_form and tag in self.INPUTS:
-            self.addInput(attrs)
+            self._add_input(attrs)
         elif tag == 'form':
             self.in_form = True
-            self.addForm(attrs)
+            self._add_form(attrs)
 
     def handle_endtag(self, tag):
         if self.in_form and tag=='form':
@@ -62,4 +96,4 @@ class FormHtmlParser(HTMLParser):
 
     def handle_startendtag(self, tag, attrs):
         if self.in_form and tag in self.INPUTS:
-            self.addInput(attrs)
+            self._add_input(attrs)
